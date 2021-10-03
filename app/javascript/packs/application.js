7b4e2d06eef79200252dc92bf76c550bfbdac971
鷲見和希 <cs5200@icloud.com>
2021-10-03 23:07:24 +0900

    cssのpath変更

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,7 +10,7 @@ require("channels")
 require('bootstrap/dist/js/bootstrap.min.js')
 
 import "bootstrap"
-import "../src/application.scss"
+import "../styles/application.scss"
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
