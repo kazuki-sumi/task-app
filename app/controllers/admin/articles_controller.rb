0cdb93aead019f1f2eb50eb6a1ef5a1522e9b355
鷲見和希 <cs5200@icloud.com>
2021-10-24 22:16:45 +0900

    Admin::ArticlesController追加

--- app/controllers/admin/articles_controller.rb
+++ app/controllers/admin/articles_controller.rb
@@ -1,22 +1,47 @@
-class Admin::ArticlesController < ApplicationController
+class Admin::ArticlesController < Admin::ApplicationController
   def index
+    @articles = Article.all.page(params[:page])
   end
 
   def show
+    @article = Article.find(params[:id])
   end
 
   def new
+    @article = Article.new
   end
 
   def create
+    @article = Article.new(form_params)
+    if @article.save
+      redirect_to :admin_articles_path
+    else
+      render :new
+    end
   end
 
   def edit
+    @article = Article.find(params[:id])
   end
 
   def update
+    @article = Article.find(params[:id])
+    if @article.update(form_params)
+      redirect_to :admin_articles_path
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @article = Article.find(params[:id])
+    @article.destroy!
+    redirect_to :admin_articles_path
+  end
+
+  private
+
+  def form_params
+    params.require(:form).permit(:title, :content, :published_at, :important, :status)
   end
 end
