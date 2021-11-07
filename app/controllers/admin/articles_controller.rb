ec79e280f22486f7410d31a012bdf72e5d4ac942
鷲見和希 <cs5200@icloud.com>
2021-11-07 22:07:01 +0900

    flash, errorメッセージ追加

--- app/controllers/admin/articles_controller.rb
+++ app/controllers/admin/articles_controller.rb
@@ -14,7 +14,7 @@ class Admin::ArticlesController < Admin::ApplicationController
   def create
     @article = Article.new(form_params)
     if @article.save
-      redirect_to admin_articles_path
+      redirect_to admin_articles_path, notice: "記事を作成しました。"
     else
       render :new
     end
@@ -27,7 +27,7 @@ class Admin::ArticlesController < Admin::ApplicationController
   def update
     @article = Article.find(params[:id])
     if @article.update(form_params)
-      redirect_to admin_articles_path
+      redirect_to admin_articles_path, notice: "記事を編集しました。"
     else
       render :edit
     end
@@ -36,7 +36,7 @@ class Admin::ArticlesController < Admin::ApplicationController
   def destroy
     @article = Article.find(params[:id])
     @article.destroy!
-    redirect_to :admin_articles_path
+    redirect_to :admin_articles_path, notice: "記事を削除しました。"
   end
 
   private
